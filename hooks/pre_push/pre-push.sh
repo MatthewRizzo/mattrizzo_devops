70bbb4a6d066f4fa3dcde5d164d5747e4f0518d4
Matt Rizzo <mattrizzo21@gmail.com>
2022-10-26 13:27:18 -0400

    Updated pre-push.sh and pre-commit mypy entry

--- hooks/pre_push/pre-push.sh
+++ hooks/pre_push/pre-push.sh
@@ -3,8 +3,19 @@
 # THIS IS SYMBOLICALLY LINKED FROM mattrizzo_devops.
 # USE setup_cloned_repo TO COPY THE SCRIPT FROM mattrizzo_devops/hooks/pre_push/pre-push.sh
 
+# Script responsible for setting up hooks in a given respository
+SCRIPTPATH=""
+if [[ "$0" == *"/usr/bin"* ]]; then
+    # Script is being sourced
+    SCRIPTPATH="$( cd -- "$(dirname "${BASH_SOURCE}")" >/dev/null 2>&1 ; pwd -P )"
+else
+    # Script is being executed
+    SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+fi
+DEVOPS_TOP_DIR="$(cd ${SCRIPTPATH} && git rev-parse --show-toplevel 2> /dev/null)"
+
 #### Inspired by pre-commit template
-INSTALL_PYTHON=/usr/bin/python
+INSTALL_PYTHON=/usr/bin/python3.10
 ARGS=(hook-impl --config=.pre-commit-config.yaml --hook-type=pre-push)
 # end templated
 
@@ -16,7 +27,7 @@ ARGS+=(--hook-dir "$HERE" -- "$@")
 repo_top_dir="$(git rev-parse --show-toplevel 2> /dev/null)"
 
 function check_rust(){
-    ${gists_location}/git/hooks/pre_push/check_rust.py \
+    ${DEVOPS_TOP_DIR}/hooks/pre_push/check_rust.py \
         --cwd ${repo_top_dir} \
         --verbose
     local -r return_code=$?
