7fbe5763cbb20cbbc449bc5e4107fcce47a54695
Matt Rizzo <mattrizzo21@gmail.com>
2022-11-30 15:13:17 -0500

    Potential mypy fix

--- hooks/pre_push/run_mypy.py
+++ hooks/pre_push/run_mypy.py
@@ -13,11 +13,11 @@ constants.SUCCESS_CODE = 0
 constants.FAILURE_CODE = 1
 
 
-def run_mypy(module: str, repo_top_dir: Path, is_verbose: bool) -> int:
-    """Runs mypy on the given module and returns if it succeeded or not"""
+def run_mypy(package: str, repo_top_dir: Path, is_verbose: bool) -> int:
+    """Runs mypy on the given package and returns if it succeeded or not"""
     try:
         mypy_res = check_output(
-            ["mypy", "-p", module], stderr=STDOUT, cwd=repo_top_dir, encoding="utf-8"
+            ["mypy", "-p", package], stderr=STDOUT, cwd=repo_top_dir, encoding="utf-8"
         )
         print(mypy_res)
         if mypy_res == constants.FAILURE_CODE:
@@ -32,7 +32,7 @@ def run_mypy(module: str, repo_top_dir: Path, is_verbose: bool) -> int:
 
 
 def check_mypy() -> int:
-    """Uses CLI args to run mypy on all given modules"""
+    """Uses CLI args to run mypy on the given package"""
     parser = argparse.ArgumentParser()
 
     repo_top_dir = get_repo_top_dir()
@@ -41,7 +41,7 @@ def check_mypy() -> int:
         "--verbose",
         help="Set to make verbose",
         action="store_true",
-        default=False,
+        default=True,
     )
     parser.add_argument(
         "-p", "--package", help="The package to run through mypy", type=str, default=""
@@ -51,10 +51,10 @@ def check_mypy() -> int:
     args, _unknown_args = parser.parse_known_args()
 
     if args.package == "":
-        if args.verbose:
-            print("No package given with '-p | --packages'. Skipping this check")
-        return constants.SUCCESS_CODE
+        print("No package given with '-p | --packages'. Skipping this check")
+        return constants.FAILURE_CODE
 
+    print(f"Running mypy for {args.package}")
     overall_res = constants.SUCCESS_CODE
     overall_res &= run_mypy(args.package, repo_top_dir, args.verbose)
 
