988b146a5715626e3f45aff3983272729424bd81
Matt Rizzo <mattrizzo21@gmail.com>
2025-08-30 20:11:57 -0400

    Remove mlocate

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,7 +37,6 @@ function install_system_packages(){
         libspa-0.2-bluetooth \
         pipewire \
         pipewire-audio-client-libraries \
-        mlocate \
         pandoc \
         groff \
         ghostscript \
